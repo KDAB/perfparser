deff2b94c906e8d55bc120aa6e7c9c33a0fb1590
Ulf Hermann <ulf.hermann@theqtcompany.com>
2015-01-27 13:16:37 +0100

    Add license headers
    Change-Id: I33a069928a1d481c1fa99e9760c9d3a5d7ea99b3
    Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

--- perffeatures.h
+++ perffeatures.h
@@ -1,3 +1,23 @@
+/****************************************************************************
+**
+** Copyright (C) 2015 The Qt Company Ltd
+** All rights reserved.
+** For any questions to The Qt Company, please use contact form at http://www.qt.io/contact-us
+**
+** This file is part of the Qt Enterprise Perf Profiler Add-on.
+**
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU General Public License
+** version 3 as published by the Free Software Foundation and appearing in
+** the file LICENSE.GPLv3 included in the packaging of this file. Please
+** review the following information to ensure the GNU General Public License
+** requirements will be met: https://www.gnu.org/licenses/gpl.html.
+**
+** If you have questions regarding the use of this file, please use
+** contact form at http://www.qt.io/contact-us
+**
+****************************************************************************/
+
 #ifndef PERFFEATURE_H
 #define PERFFEATURE_H
 
