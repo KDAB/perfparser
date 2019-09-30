f010f12d68fa5269e6b7e16b8c2c2dffcb97a26c
Ulf Hermann <ulf.hermann@qt.io>
2019-09-30 11:16:16 +0200

    Bump application version to 4.11
    Change-Id: I222cf66c4555c99bdb56e48fd1383c9ebfb7def7
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- app/main.cpp
+++ app/main.cpp
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
     app.setApplicationName(QLatin1String("perfparser"));
-    app.setApplicationVersion(QLatin1String("4.10"));
+    app.setApplicationVersion(QLatin1String("4.11"));
 
     if (qEnvironmentVariableIsSet("PERFPARSER_DEBUG_WAIT")) {
 #ifdef Q_OS_LINUX
