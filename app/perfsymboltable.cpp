7c56e85bbbf754bc8ca160311201c2d85f32023f
Milian Wolff <milian.wolff@kdab.com>
2020-06-13 13:34:37 +0200

    Don't try to open perf.map file when it doesn't exist
    Removes a lot of open() syscalls that are bound to fail anyways.
    
    Change-Id: I1b1d6d574dd5148922630aa84b8996b6b9d55af7

--- app/perfsymboltable.cpp
+++ app/perfsymboltable.cpp
@@ -1070,6 +1070,9 @@ QByteArray PerfSymbolTable::symbolFromPerfMap(quint64 ip, GElf_Off *offset) cons
 
 void PerfSymbolTable::updatePerfMap()
 {
+    if (!m_perfMapFile.exists())
+        return;
+
     if (!m_perfMapFile.isOpen())
         m_perfMapFile.open(QIODevice::ReadOnly);
 
