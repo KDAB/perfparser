2faa999097030495a8b71f89a7207c0be8734cb8
Mimir Reynisson <rimim@users.noreply.github.com>
2025-02-20 12:09:12 -0500

    Update swift_demangle.h

--- app/swift_demangle.h
+++ app/swift_demangle.h
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with [project name].  If not, see <https://www.gnu.org/licenses/>.
+ * along with perfparser.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef SWIFT_DEMANGLER_H
