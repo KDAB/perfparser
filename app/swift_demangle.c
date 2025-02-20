e7aec07352684ceac207c32b769eaf36719b75a0
Mimir Reynisson <rimim@users.noreply.github.com>
2025-02-20 12:08:59 -0500

    Update swift_demangle.c
    Updated copyright notice

--- app/swift_demangle.c
+++ app/swift_demangle.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2025 Mimir Reynissonr
+ *
+ * This file is part of perfparser.
+ *
+ * perfparser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * perfparser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with perfparser.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "swift_demangler.h"
 #include <stdlib.h>
