f4f69b7a5fc9a807131412da3e2fcaacaf194c58
Mimir Reynisson <rimim@users.noreply.github.com>
2025-02-20 12:08:33 -0500

    Update swift_demangle.h
    Updated copyright notice

--- app/swift_demangle.h
+++ app/swift_demangle.h
@@ -1,6 +1,25 @@
+/*
+ * Copyright (C) 2025 Mimir Reynissonr
+ *
+ * This file is part of perfparser.
+ *
+ * perfparser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * perfparser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with [project name].  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef SWIFT_DEMANGLER_H
 #define SWIFT_DEMANGLER_H
 
-int swift_demangle(const char* symbol, char* buffer, size_t bufferLength) {
+int swift_demangle(const char* symbol, char* buffer, size_t bufferLength);
 
 #endif
